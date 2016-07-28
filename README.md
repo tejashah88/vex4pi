b79b06c7d9ec96b7b7a8434eff6006fb75b28312
Tejas Shah <tejashah88@gmail.com>
2016-07-28 02:36:41 -0700

    revamped library: no more OOP style code

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # vex4pi
 A JavaScript library allowing the raspberry pi to control VEX motors via the Adafruit Servo/PWM HAT!
 
-## Code Example
+# Disclaimer
+* This library is intended to work only for the Raspberry PI, and has **NOT** been tested with other hardware platforms.
+* This library requires a Adafruit Servo/PWM HAT, and will **NOT** work on direct GPIO.
+* This library only supports VEX Motors at of this release, and has not been tested with other VEX hardware, such as the VEX Servos.
+* This library only contains sync functions. Async may be implemented in a later version.
 
-## Installation
 
-## API Reference
+# Installation
 
-## Final Notes
+```
+npm install vex4pi --save
+```
+
+# Features
+
+# Code Example
+
+# API Reference
--- vex4pi.js
+++ vex4pi.js
@@ -1,54 +1,15 @@
-/*
- * A JavaScript library allowing the raspberry pi to control VEX motors
- * via the Adafruit Servo/PWM HAT!
- */
-
 // Import the needed modules and initialize the needed vars
 var i2c = require('i2c-bus');
 var bus; // the bridge to directly control the vex motors
-var pins = []; // used to maintain active list of pins used up; range = 0-15
 var ready = false; // used to see if the PWM HAT is ready to send commands
+var vex4pi = {};
 
-// Utility methods
-
-//checks if the variable being checked is a legit integer
-function isInteger(n){
-  return ((typeof n == "number") && !isNaN(n) && (parseFloat(n) == parseInt(n)));
+var broadcastError = function(msg) {
+	vex4pi.deinit();
+	return new Error(msg);
 }
 
-// used for checking if an array has a certain value
-Array.prototype.contains = function(obj) {
-    var i = this.length;
-    while (i--) {
-        if (this[i] === obj) {
-            return true;
-        }
-    }
-    return false;
-}
-
-// Main class to manipulate the vex motors
-function VexMotor(pin) {
-	// check if the i2c-bus is ready
-	if (!ready) {
-		throw new Error("I2C bus connection is not initialized with VexMotor.init()");
-	// do some type checking
-	} else if (!isInteger(pin)) {
-		throw new Error("Pin value " + pin + " is invalid! Use an integer.");
-	} else if (pin < 0 || pin > 15) { // check if the pin value given is within range
-		throw new Error("Pin value " + pin + " is out of range of 0 to 15!");
-	// check if the pin specified is not used
-	} else if (pins.contains(pin)) {
-		throw new Error("Pin value " + pin + " is already in use!");
-	// we are good to go
-	} else {
-		this.pin = pin;
-		pins.push(pin);
-		this.setSpeed(0);
-	}
-}
-
-exports.init = function() {
+vex4pi.init = function() {
 	bus = i2c.openSync(1);
 	bus.writeByteSync(0x40,0x00,0x10); // sleep mode
 	bus.writeByteSync(0x40,0xFE,0x85); // set to ~49.5Hz
@@ -58,9 +19,9 @@ exports.init = function() {
 	ready = true; // mark the 'ready' flag to true to allow access
 };
 
-exports.deinit = function() {
-	if (pins.length > 0) {
-		throw new Error("Warning: there are still VexMotor objects that haven't been closed properly! Pins " + pins + " are still in use.");
+vex4pi.deinit = function() {
+	for (var pin = 0; pin < 16; pin++) {
+		vex4pi.setSpeed(pin, 0);
 	}
 	
 	ready = false;	// we mark the 'ready' flag false first, in order to prevent
@@ -68,15 +29,15 @@ exports.deinit = function() {
 	bus.closeSync();
 }
 
-VexMotor.prototype.setSpeed = function(speed) {
+vex4pi.setSpeed = function(pin, speed) {
 	if (!ready) {
-		throw new Error("I2C bus connection is not initialized with VexMotor.init()");
-	// do some type checking
-	} else if (!isInteger(speed)) {
-		throw new Error("Speed value " + pin + " is invalid! Use an integer.");
-	} else if (speed < -100 || speed > 100) { // check if the pin value given is within range
-		throw new Error("Speed value " + pin + " is out of range of -100 to 100!");
-	// check if the pin specified is not used
+		throw broadcastError("I2C bus connection is not initialized with vex4pi.init()");
+	// check if the pin value given is within range
+	} else if (pin < 0 || pin > 15) {
+		throw broadcastError("Pin value " + pin + " is out of range.");
+	// check if the speed value given is within range
+	} else if (speed < -100 || speed > 100) {
+		throw broadcastError("Speed value " + speed + " is out of range.");
 	}
 	
 	//initial configuration
@@ -94,9 +55,4 @@ VexMotor.prototype.setSpeed = function(speed) {
 	bus.writeByteSync(0x40, 0x09 + (this.pin * 0x04), speedHSB); // high part of data
 }
 
-VexMotor.prototype.detach = function() {
-	this.setSpeed(0);
-	pins = pins.filter(e => e !== this.pin);
-}
-
-exports.VexMotor = VexMotor;
+module.exports = vex4pi;
