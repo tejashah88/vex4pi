49f760c74bd48ad7158c3250433d4440d7a46be3
Tejas Shah <tejashah88@gmail.com>
2016-07-29 00:16:47 -0700

    pins will reset during BOTH init and deinit

--- vex4pi.js
+++ vex4pi.js
@@ -1,12 +1,18 @@
 // Import the needed modules and initialize the needed vars
 var i2c = require('i2c-bus');
-var bus; // the bridge to directly control the vex motors
-var ready = false; // used to see if the PWM HAT is ready to send commands
-var vex4pi = {};
+var bus; 			// the bridge to directly control the vex motors
+var ready = false; 	// used to see if the PWM HAT is ready to send commands
+var vex4pi = {}; 	// the container for all the to-be exported functions and variables 
 
 var broadcastError = function(msg) {
 	vex4pi.deinit();
 	return new Error(msg);
+}
+
+var resetAllPins = function() {
+	for (var pin = 0; pin < 16; pin++) {
+		vex4pi.setSpeed(pin, 0);
+	}
 }
 
 vex4pi.init = function() {
@@ -16,16 +22,19 @@ vex4pi.init = function() {
 	bus.writeByteSync(0x40,0x00,0x00); // wake up
 	bus.writeByteSync(0x40,0x01,0x04); // output enable
 	
+	// reset the pins to avoid surprises from the motors
+	resetAllPins();
+	
 	ready = true; // mark the 'ready' flag to true to allow access
 };
 
 vex4pi.deinit = function() {
-	for (var pin = 0; pin < 16; pin++) {
-		vex4pi.setSpeed(pin, 0);
-	}
+	ready = false;	// we mark the 'ready' flag false first, in order to prevent accidental access during the procedure
 	
-	ready = false;	// we mark the 'ready' flag false first, in order to prevent
-					// accidental access during the procedure
+	// reset the motor speed to neutral before closing the connection with the bus
+	resetAllPins();
+	
+	// close the connection with the bus
 	bus.closeSync();
 }
 
@@ -36,8 +45,12 @@ vex4pi.setSpeed = function(pin, speed) {
 	} else if (pin < 0 || pin > 15) {
 		throw broadcastError("Pin value " + pin + " is out of range.");
 	// check if the speed value given is within range
-	} else if (speed < -100 || speed > 100) {
-		throw broadcastError("Speed value " + speed + " is out of range.");
+	} else {
+		if (speed < -100) {
+			speed = -100;
+		} else if (speed > 100) {
+			speed = 100;
+		}
 	}
 	
 	//initial configuration
